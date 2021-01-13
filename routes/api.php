f0010bcecb53925b94135feda91b3a0ff5c38091
Daniel França <daniel.franca@ativas.com.br>
2021-01-13 00:42:21 -0300

    Criando rota

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', 'HomeController@index');
+    Route::get('/get-flights', 'FlightGroupingController@index');
+    Route::get('/flights/grouping', 'FlightGroupingController@group');
+});
