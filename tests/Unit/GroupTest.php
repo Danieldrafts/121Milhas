5ce9deff9aada1e57e34b0234227abc301c1560f
Daniel França <daniel.franca@ativas.com.br>
2021-01-13 19:31:23 -0300

    Criando testes

---
+++ tests/Unit/GroupTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Resources\Group;
+
+
+class GroupTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    /**
+    * @expectedException Exception
+    */
+
+    public function testTotalPrice()
+    {
+        $group = new Group();
+        $group->getTotalPrice();
+    }
+}
